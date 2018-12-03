dbef43b8857b91c7e02d948a0dfe664c967fda05
Melle Meewis <mellemeewis@icloud.com>
2018-12-03 09:42:13 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Melle Meewis on 03/12/2018.
+//  Copyright © 2018 Melle Meewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
